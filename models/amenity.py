8711a996327eee1eff84389ab27707b722ec8e85
error-rgl <rglcnd@gmail.com>
2022-03-06 01:02:56 +0100

    [:Feature:] Class Amenity that inherits from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Amenity class!
+    Attribute:
+        name (str): name of amenity
+    """
+    name = ""
