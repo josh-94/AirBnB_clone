6ded8d89a72cf241dc0606037f8cefea082b25d0
error-rgl <rglcnd@gmail.com>
2022-03-06 01:34:58 +0100

    [:Feature:] Class State that inherits from BaseModel

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines state class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ State class!
+    Attribute:
+        name (str): name of state
+    """
+    name = ""
